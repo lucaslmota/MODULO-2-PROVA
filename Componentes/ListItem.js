181a2e72a52481d31d26c034e63a05bd06301a8d
lucaslmota <lucaslmota0431@gmail.com>
2022-05-31 22:39:29 -0300

    MODULO 2 PROVA

---
+++ Componentes/ListItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { TouchableOpacity, View, Image, Text, StyleSheet } from 'react-native';
+
+const ListItem = ({ data }) => {
+    return (
+        <TouchableOpacity style={styles.item}>
+            <Image source={{ uri: data.avatar }} style={styles.itemPhoto} />
+            <View style={styles.itemInfo}>
+                <Text style={styles.itemP1}>Autor: {data.name}</Text>
+                <Text style={styles.itemP2}>Titulo: {data.title}</Text>
+                <Text style={styles.itemP2}>URL: {data.url}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    item: {
+        flexDirection: 'row',
+        marginLeft: 30,
+        marginRight: 30,
+        borderBottomWidth: 1,
+        borderBottomColor: '#444',
+        paddingTop: 15,
+        paddingBottom: 15,
+    },
+    itemPhoto: {
+        width: 50,
+        height: 50,
+        borderRadius: 30,
+    },
+    itemInfo: {
+        marginLeft: 20,
+    },
+    itemP1: {
+        fontSize: 22,
+        color: '#FFFFFF',
+        marginBottom: 5
+    },
+    itemP2: {
+        fontSize: 18,
+        color: '#999999',
+    },
+});
+
+export default ListItem;
